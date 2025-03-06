f977c9ccd11984573aec48b94f834256163b2968
hhyufan <1838248655@qq.com>
2025-03-06 14:32:39 +0800

    fix: 修改远程端错误的localDebug状态

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import store, {setBaseURL, setPublicKey} from "@/store/store.js";
 
-const localDebug = true;
+const localDebug = false;
 const REMOTE_URL = localDebug ? "http://localhost:8088" : 'https://yuki.cmyam.net/miaogu_api';
 
 const REMOTE_API_URL = `${REMOTE_URL}/edge-config/api/config`;
