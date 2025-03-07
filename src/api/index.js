7e7bd28f72d66e4f73af8cfb27271c1ceb178c43
hhyufan <1838248655@qq.com>
2025-03-07 13:34:34 +0800

    chore: 改用import.meta.env编写环境变量，不再使用edge-config

--- src/api/index.js
+++ src/api/index.js
@@ -1,41 +1,33 @@
 import store, {setBaseURL, setPublicKey} from "@/store/store.js";
 
-const localDebug = false;
-const REMOTE_URL = localDebug ? "http://localhost:8088" : 'https://yuki.cmyam.net/miaogu_api';
-
-const REMOTE_API_URL = `${REMOTE_URL}/edge-config/api/config`;
-const API_URL = '/api/config';
-
-async function fetchConfig(url) {
-    try {
-        const response = await fetch(url);
-        return await response.json();
-    } catch (error) {
-        console.error('Failed to fetch config:', error);
-        throw error; // 重新抛出错误以便调用者处理
+const requireEnv = (variableName) => {
+    const value = import.meta.env[variableName];
+    if (!value) {
+        throw new Error(`${variableName} Not Empty!`);
     }
+    return value;
 }
+
+const REMOTE_URL = requireEnv('VITE_REMOTE_URL');
+const LOCAL_HOST = requireEnv('VITE_LOCAL_HOST');
+const PUBLIC_KEY = requireEnv('VITE_PUBLIC_KEY');
 
 export async function getBaseUrl() {
     const state = store.getState();
-
     if (state.edgeConfig.baseURL) return state.edgeConfig.baseURL; // 缓存结果
-    const apiUrl = REMOTE_URL
-    store.dispatch(setBaseURL(apiUrl));
-    return apiUrl; // 返回更新后的 baseUrl
+
+    const isLocalhost = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
+    const baseUrl = isLocalhost ? LOCAL_HOST : REMOTE_URL
+
+    store.dispatch(setBaseURL(baseUrl));
+    return baseUrl; // 返回更新后的 baseUrl
 }
 
 export async function getPublicKey() {
     const state = store.getState();
     if (state.edgeConfig.publicKey) return state.edgeConfig.publicKey; // 缓存结果
-
-    const isLocalhost = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
-    const url = isLocalhost ? REMOTE_API_URL : API_URL;
-
-    const data = await fetchConfig(url);
-    const publicKey = isLocalhost ? data.find(item => item.key === 'VUE_APP_PUBLIC_KEY')?.value : data?.publicKey;
-    store.dispatch(setPublicKey(publicKey));
-    return publicKey; // 返回更新后的 publicKey
+    store.dispatch(setPublicKey(PUBLIC_KEY));
+    return PUBLIC_KEY; // 返回更新后的 publicKey
 }
 
 // 在应用启动时初始化 baseUrl
